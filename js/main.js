41041b1f08036ec071b0ef05583d343704a9c177
GCaringi <giannicaringi94@gmail.com>
2022-05-24 01:07:16 +0200

    Add remove and done

--- js/main.js
+++ js/main.js
@@ -4,21 +4,37 @@ const app = new Vue({
         todos: [
             {
                 text: "Fare esercizio",
-                done: "false",
+                done: false
             },
             {
                 text: "Lavare l'auto",
-                done: "false"
+                done: true
             }
         ],
         newTask: ""
     },
     methods: {
         addTask(){
-            this.todos.push({
-                    text: this.newTask,
-                    done: "false"})
-            this.newTask = "";
+            let regex = /^\S(\w+)(( ){1}(\w+))*\S*/g;
+            newTask = this.newTask.trim();
+            if (regex.test(this.newTask)){
+                this.todos.push(
+                    {
+                        text: this.newTask,
+                        done: "false;"
+                    }
+                );
+                this.newTask = "";
+            }else{
+                this.newTask = "";            }
+        },
+        removeTask(index){
+            console.log(index);
+            this.todos.splice(index, 1);
+        },
+        checkTask(index){
+            console.log(index);
+            this.todos[index].done = !this.todos[index].done;
         }
-    },
+    }
 })
