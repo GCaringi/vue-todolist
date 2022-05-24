1d4cbfa18470b673dafbed7bf6bf3894c9942b0b
GCaringi <giannicaringi94@gmail.com>
2022-05-24 02:26:30 +0200

    Add some style

--- js/main.js
+++ js/main.js
@@ -2,25 +2,31 @@ const app = new Vue({
     el: "#app",
     data: {
         todos: [
-            {
-                text: "Fare esercizio",
-                done: false
-            },
-            {
-                text: "Lavare l'auto",
-                done: false
-            }
+             {
+                 text: "Fare esercizio",
+                 done: false
+             },
+             {
+                 text: "Lavare l'auto",
+                 done: false
+             }
         ],
         newTask: ""
     },
     methods: {
+        firstToUpper(str){
+            let newStr = "";
+            newStr += str[0].toUpperCase();
+            newStr += str.slice(1);
+            return newStr;
+        },
         addTask(){
-            let regex = /^\S(\w+)(( ){1}(\w+))*\S*/g;
+            let regex = /^\S(\w)*(( ){1}(\w+))*\S*/g;
             newTask = this.newTask.trim();
             if (regex.test(this.newTask)){
                 this.todos.push(
                     {
-                        text: this.newTask,
+                        text: this.firstToUpper(this.newTask),
                         done: false
                     }
                 );
